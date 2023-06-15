a97dc3dbd858b8b391e4d6526d89dc95812ecedc
lorisohr <lyrigs@gmail.com>
2023-06-15 10:15:09 +0200

    main component started

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,6 +7,15 @@ const Main = () => {
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-400">
             LET'S BUILD SOMETHING TOGETHER
+            <h1>
+              Hi, I'm <span className="text-[#5651e5]"> Loris </span>{" "}
+            </h1>
+            <h1 className="py-2">A Business Informatics Student</h1>
+            <p className="py-4 max-w-[70%] m-auto">
+              I'm a business informatics student that is currently in his 4th
+              year. my plan is to get my matura from my current school and then
+              go to the technical university in vienna to study informatics
+            </p>
           </p>
         </div>
       </div>
