54b6297abd06d85a3e1511ad11f1e287d7f00f68
lorisohr <lyrigs@gmail.com>
2023-06-15 10:23:24 +0200

    main component icons

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -7,16 +10,30 @@ const Main = () => {
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-400">
             LET'S BUILD SOMETHING TOGETHER
-            <h1>
-              Hi, I'm <span className="text-[#5651e5]"> Loris </span>{" "}
-            </h1>
-            <h1 className="py-2">A Business Informatics Student</h1>
-            <p className="py-4 max-w-[70%] m-auto">
-              I'm a business informatics student that is currently in his 4th
-              year. my plan is to get my matura from my current school and then
-              go to the technical university in vienna to study informatics
-            </p>
           </p>
+          <h1>
+            Hi, I'm <span className="text-[#5651e5]"> Loris </span>{" "}
+          </h1>
+          <h1 className="py-2">A Business Informatics Student</h1>
+          <p className="py-4 max-w-[70%] m-auto">
+            I'm a business informatics student that is currently in his 4th
+            year. My plan is to get my matura from my current school and then go
+            to the technical university in Vienna to study informatics.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-[#212129] p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-[#212129] p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-[#212129] p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-[#212129] p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
         </div>
       </div>
     </div>
